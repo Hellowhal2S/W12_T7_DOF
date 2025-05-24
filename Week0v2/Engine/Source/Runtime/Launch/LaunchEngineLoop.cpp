751595a4317c8e3afd8bf1b9f7a0fb7c56f0c799
jungjihyung <wlgud3936@gmail.com>
2025-05-24 21:08:51 +0900

    feat: Create함수 추가, Simulate함수 적용

--- Week0v2/Engine/Source/Runtime/Launch/LaunchEngineLoop.cpp
+++ Week0v2/Engine/Source/Runtime/Launch/LaunchEngineLoop.cpp
@@ -47,6 +47,8 @@ int32 FEngineLoop::Init(const HINSTANCE hInstance)
 
     GEngine->Init();
     FPhysX::InitPhysX();
+    // 박스 생성
+    gObjects.push_back(FPhysX::CreateBox(PxVec3(0, 5, 0), PxVec3(1,1,1)));
     for (const auto& AppWnd : AppWindows)
     {
         UpdateUI(AppWnd);
@@ -82,9 +84,12 @@ void FEngineLoop::Tick()
             }
         }
         const float DeltaTime = static_cast<float>(ElapsedTime / 1000.f);
-
+        
         GEngine->Tick(DeltaTime);
 
+        // 시뮬레이트
+        FPhysX::Simulate(1.0f / 60.0f);
+        
         Render();
 
         GUObjectArray.ProcessPendingDestroyObjects();
